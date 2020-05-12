d1fa585deb3b73669088ff71654f806c4e40f521
Daniel Son <dson1204@gmail.com>
2020-05-12 22:20:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ function decrement(n) {
   return n -= 1;
 }
 function makeInt(n) {
-  return parseInt('n', 10)
+  return parseInt('n', 10);
 }
-function makeInt(string) {
-  return parseInt('string', 10)
 }
